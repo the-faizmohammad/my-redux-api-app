b4de6283f3b1cf8e4ef57d67ab824a06711a54fe
user-faiz <email2faizm@gmail.com>
2023-09-16 10:50:54 +0530

    update userSlice.js

---
+++ src/store/users/userSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  users: [],
+  isLoading: false,
+  error: null,
+};
+
+// Create an async action creator to fetch users from the API
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  try {
+    const response = await fetch('https://randomuser.me/api/?results=5');
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    throw error;
+  }
+});
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default usersSlice.reducer;
